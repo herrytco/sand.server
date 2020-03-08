f497b5414d9ab44211b0fb8d26bb8f20b2b8b4f9
LucaBraun <44901406+LucaBraun@users.noreply.github.com>
2020-03-08 12:46:55 +0100

    !rollt and some debugging
    Multicommands now return false on any error and not only if command is invalid
    
    Multicommands can now handle no parameters
    
    rollt

--- discord/src/main/java/systems/nope/discord/DiscordConstants.java
+++ discord/src/main/java/systems/nope/discord/DiscordConstants.java
@@ -1,5 +1,6 @@
 package systems.nope.discord;
 
 public final class DiscordConstants {
-    static final String token = "NjQ3NTMwMjUwNDMwOTcxOTA2.XfPPiA.HYW2LnYaM4r-gWh3W6ZlMulJeEo";
+    static final String token = "NjM2OTIzMTIzMjMyNTM4NjI0.XmSQnA.hzytZRPJMzKApgMbD-1rRtSiXl0"; //nicer dicer
+    //static final String token = "NjQ3NTMwMjUwNDMwOTcxOTA2.XfPPiA.HYW2LnYaM4r-gWh3W6ZlMulJeEo"; //narrator (doesn't work)
 }
