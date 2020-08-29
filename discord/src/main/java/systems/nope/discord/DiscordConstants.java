de68e066dcc204a7ceadbaca6f2a4074dfa044da
Herry <herrytco@gmail.com>
2020-08-29 18:21:32 +0200

    added docker-compose.yml

--- discord/src/main/java/systems/nope/discord/DiscordConstants.java
+++ discord/src/main/java/systems/nope/discord/DiscordConstants.java
@@ -1,5 +1,5 @@
 package systems.nope.discord;
 
 public final class DiscordConstants {
-    static final String token = "NjQ3NTMwMjUwNDMwOTcxOTA2.XdhBig.3XRGp2wbs_ggRLKZid5YNSYRPHQ";
+    static final String token = "NjQ3NTMwMjUwNDMwOTcxOTA2.XdhBig.4BYtzAoNy4Vtm3_4p6uTrlhmy5c";
 }
--- discord/src/main/java/systems/nope/discord/eventlistener/dice/ServerConstants.java
+++ discord/src/main/java/systems/nope/discord/eventlistener/dice/ServerConstants.java
@@ -16,7 +16,7 @@ public final class ServerConstants {
 
     public static final String help = "!roll {MOD} ... rolls once with an optional modifier\n!uavg ... prints your average (which is hopefully good)\n!rollx [X] {MOD} ... rolls [X] dice with an optional modifier\n!rollx [X] [min|max] {MOD} ... rolls [X] dice and chooses the best/worst with an optional modifier.\n!rollt [target0] [X0] [target1] [X1] etc ... rolls [X] dice against each [target]\n!rollo [target0] [X0] [target1] [X1] etc ... rolls [X] dice for each [target]\n!umod ... gives you your current dice modifier\n!umod [X] ... sets the modifier for a user to [X] which gets added/subtracted to each roll\n!udice [X] ... continues to roll with an [X]-sided dice\n!rollc [X] {MODIFIER} ... rolls [X] dice and sums the result with an optional modifier\n!uavgreset ... resets your average\n!savg ... prints the average of the whole server\n!savgreset ... prints the average of the whole server\n!help ... prints the help, but I guess you knew that already\n\n!link [api-Key] ... links the discord account to your character\n!rolla [ATTRIBUTE] {MODIFIER} ... rolls once for the selected attribute with an optional modifier\n\n!party ... adds all non-bot members of your current voice channel to your party\n!proll ... rolls once for each !party member\n!prollx [X] {MODIFIER} ... rolls [X] dice for each party member with an optional modifier";
 
-    public static final String urlBackend = "http://test2.nope-api.systems:8085";
+    public static final String urlBackend = "http://localhost:80";
     public static final String narratorUsername = "narrator@nope-api.systems";
     public static final String narratorPassword = "amrWHr7lx09dny6ygb84";
 }
