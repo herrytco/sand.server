8605a06d1859b8c33ffa8e78aa7d9b000ec84cdf
Herry <herrytco@gmail.com>
2021-06-18 12:21:28 +0200

    [discord] updated backend url

--- discord/src/main/java/systems/nope/discord/constants/ServerConstants.java
+++ discord/src/main/java/systems/nope/discord/constants/ServerConstants.java
@@ -10,7 +10,7 @@ public final class ServerConstants {
 
     public static final String help = "BOT COMMANDS:\n!roll {MOD} ... rolls once with an optional modifier\n!uavg ... prints your average (which is hopefully good)\n!rollx [X] {MOD} ... rolls [X] dice with an optional modifier\n!rollx [X] [min|max] {MOD} ... rolls [X] dice and chooses the best/worst with an optional modifier.\n!rollt [target0] [X0] [target1] [X1] etc ... rolls [X] dice against each [target]\n!rollo [target0] [X0] [target1] [X1] etc ... rolls [X] dice for each [target]\n!umod ... gives you your current dice modifier\n!umod [X] ... sets the modifier for a user to [X] which gets added/subtracted to each roll\n!udice [X] ... continues to roll with an [X]-sided dice\n!rollc [X] {MODIFIER} ... rolls [X] dice and sums the result with an optional modifier\n!uavgreset ... resets your average\n!savg ... prints the average of the whole server\n!savgreset ... prints the average of the whole server\n!help ... prints the help, but I guess you knew that already\n\n!link [api-Key] ... links the discord account to your character\n!rolla [ATTRIBUTE] {MODIFIER} ... rolls once for the selected attribute with an optional modifier\n\n!party ... adds all non-bot members of your current voice channel to your party\n!proll ... rolls once for each !party member\n!prollx [X] {MODIFIER} ... rolls [X] dice for each party member with an optional modifier";
 
-    private static String hostBackend = "http://test.nope-api.systems:30005";
+    private static String hostBackend = "https://test.nope-api.systems:30005";
 
     public static void setHostBackend(String host) {
         System.out.printf("Setting backend host to: '%s'%n", host);
